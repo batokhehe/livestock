e5f74103944c400dc7f1c88f09f7c194a623e3a5
batokhehe <dhikanaufal@gmail.com>
2026-01-19 09:15:38 +0700

    Delete mvvm_flutter_boilerplate directory

--- mvvm_flutter_boilerplate/lib/core/db/connection/connection_web.dart
+++
@@ -1,8 +0,0 @@
-import 'package:drift/drift.dart';
-import 'package:drift/web.dart';
-
-LazyDatabase openConnection() {
-  return LazyDatabase(() async {
-    return WebDatabase('app_db');
-  });
-}
--- mvvm_flutter_boilerplate/lib/core/db/tables/product_table.dart
+++
@@ -1,9 +0,0 @@
-import 'package:drift/drift.dart';
-
-@DataClassName('ProductData')
-class Products extends Table {
-  IntColumn get id => integer().autoIncrement()();
-  TextColumn get name => text()();
-  RealColumn get price => real()();
-  DateTimeColumn get createdAt => dateTime().withDefault(currentDateAndTime)();
-}
